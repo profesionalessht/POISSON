3d22a44a45180443b861f237f98f166802c7dcd8
profesionalessht <75761688+profesionalessht@users.noreply.github.com>
2020-12-18 19:34:49 -0500

    Update POISSON.m
    Hola, hasta acá se encuentra el código. Como te dije es cuestión de encontrar el error de la matriz. El programa ya imprime lo que tiene que imprimir pero toca corregir algo que hace que los resultados no sean los mismos que en el ejemplo del libro. Muchas gracias y espero sirva de algo mi ayuda en la realización del algoritmo.

--- POISSON.m
+++ POISSON.m
@@ -3,42 +3,17 @@ clear all
 clc
 
 
-Ufront= @(x,y)     exp(x)*sin(y); 
-Qfront= @(x,y)     exp(x)*sin(y)*x+ exp(x)*cos(y)*y; 
+ 
 const=-2;
 
 NN=16; % Número de nodos en la frontera.
 NE=16; % Número de elementos en la frontera.
 L=17; % Número de nodos internos.
-N=NN; % 
 NCI=48; % Número de celdas internas (unicamente en el cado de Uxx+Uyy=b)
 NPI=7; % Número de puntos de integración para las celdas internas.
 NI=4; % Número de puntos de integración para los elementos de la frontera.
-VAL=1; % % Selecciona el tipo de condición de frontera VAL=1- q conocido, q desconocido; VAL=2- q conocido, u desconocido.
+VAL=2; % % Selecciona el tipo de condición de frontera VAL=1- q conocido, q desconocido; VAL=2- q conocido, u desconocido.
 
-% Creación de vectores y matrices para guardar datos.
-% X=zeros(1,N);
-% Y=zeros(1,N);
-U=zeros(1,N);
-Q=zeros(1,N);
-
-
-
-D=zeros(1,NN);
-LE=zeros(1,NN);
-CON=zeros(NN,2);
-KODE=zeros(1,NN);
-cell=zeros(1,NCI);
-
-
-alpha=zeros(1,N);
-XY=zeros(1,NN);
-A=zeros(N,N);
-MKJ=zeros(N,3);
-
-
-DA=zeros(1,N);
-CP=zeros(7,3);
 
 % Contantes para la integración sobre elementos
 POEI=[0.86113631,-0.86113631,0.33998104,-0.33998104];
@@ -87,7 +62,7 @@ for I=NN+1:NN+L
     KODE(I)=0;
     U(I)=0;
     Q(I)=0;          
-    
+     
 end
 
 
@@ -119,27 +94,32 @@ for J=1:NCI
     DA(J)=abs((AB1*AB5-AB2*AB4)/2);
     
 end
- 
+
+
 for II=1:NN  % Selecciona el tipo de condición de frontera 1- q conocido, q desconocido; 2- q conocido, u desconocido.
-        
+    
     KODE(II)=VAL;
     
-    if KODE(II)==1
+    if VAL==1
     
         U(II)=VAL;
-%     disp('KODE = 1')
+
     end
     
-    if KODE(II)==2
+    if VAL==2
+        
         Q(II)=VAL;
-%         disp('KODE =2')
+
     end
 end
     
 
 % Cálculo de la longitud de cada segmento de recta que une a cada nodo.
+
 for k=1:NE
+    
     LE(k)=sqrt((x(CON(k,2))-x(CON(k,1)))^2+(y(CON(k,2))-y(CON(k,1)))^2);
+
 end
 
 
@@ -147,23 +127,22 @@ end
 
 
 for J1=1:NN+L  % Módulo para ensamblar la matriz H y G paa reducirlo a la condición AX=Y.
+    
    XI=X(J1);
    YI=Y(J1);
    XY(J1)=0;
 
-   for J=1:NE 
+   for J=1:NN 
        
    HH(J1,J)=0;
    A(J1,J)=0;
-   cc=0;
-   
-   
+   CC=0;
    
    for J2=1:NE
        
-   LJ(J2)=LE(J2);
-   N1(J2)=CON(J2,1);
-   N2(J2)=CON(J2,2);
+   LJ=LE(J2);
+   N1=CON(J2,1);
+   N2=CON(J2,2);
    X1=X(N1);
    X2=X(N2);
    Y1=Y(N1);
@@ -173,110 +152,258 @@ for J1=1:NN+L  % Módulo para ensamblar la matriz H y G paa reducirlo a la condi
    G1=0;
    G2=0;
    
-   
-  
-
-       
-   
-    for J3=1:4  
-  
-         if J1==N1 | J1==N2
-       
-       CC=cc-H1-H2;
-       GE=LJ.*(3/2-log(LJ))./(4.*pi); % Integración exacta para G cuando I y J son el mismo elemento.
-         
-         
-         else
+    for J3=1:4  % Computo de los coeficientes G y H  
+          
             
         E = POEI(J3);
         w = FDEP(J3);
         XX = X1 + (1+E)*(X2-X1)/2;
         YY = Y1 + (1+E)*(Y2-Y1)/2;
         R = sqrt((XI-XX).^2 + (YI-YY).^2);
-        PP = ((XI-XX).*(Y1-Y2)+(YI-YY).*(X2-X1))/(R.*R*4*pi).*w;
+        PP = (((XI-XX).*(Y1-Y2)+(YI-YY).*(X2-X1))/(R.*R*4*pi))*w;
 
         % Elementos de H y G usando las formulas 2.4647 , 2.49-50
         
-        H1=H1+(1-E)*(PP/2);
-        H2=H2+(1+E)*(PP/2);
-        PP=log(1/R)./(4.*pi).*LJ.*w;
-        G1=G1+(1-E)*(PP/2);
-        G2=G2+(1+E)*(PP/2);
-        CC=cc-H1-H2;
-        GE=LJ.*(3/2-log(LJ))./(4.*pi); % Integración exacta para G cuando I y J son el mismo elemento.
-         end
+        H1=H1+((1-E)/2)*(PP);
+        H2=H2+((1+E)/2)*(PP);
+        PP=log(1/R)/(4*pi)*LJ*w;
+        G1=G1+((1-E)/2)*(PP);
+        G2=G2+((1+E)/2)*(PP);
+
+         
+        CC=CC-H1-H2;
+        GE=LJ.*((3/2)-log(LJ))./(4.*pi); % Integración exacta para G cuando I y J son el mismo elemento.
     end
+   end
+   end
+
     
     
     if N1==J1
+        % SE APLICA UNICAMENTE AL MÉTODO DRM.
+        
         G1=GE;
+        
     end
     
     if N2==J1
-        G2=GE
+        
+        G2=GE;
+        
+    end
         HH(J1,N1)=HH(J1,N1)+H1;
         HH(J1,N2)=HH(J1,N1)+H2;
         GG(J1,2*J2-1)=G1;
-        GG(J1,2*J2)=G2;
+        GG(J1,2*J2)=G2;  % ###########################################################3
+    
+end 
+% % #################################################
+% % HASTA ACA ESTOY SEGURO DE QUE ESTA PERFECTO IGUAL QUE EL LIBRO
+% % GUIA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+%     % ######################################################### DUDA
+%      
+%     
+%    end
+%    end
+% end
+% 
+for J1=1:NN
+
+    KK=KODE(N1);
+     
+    if KK==0 | KK==2
+        
+        XY(J1)=XY(J1)+Q(N1)*G1;
+        A(J1,N1)=A(J1,N1)+H1;
+    
+    else
+        XY(J1)=XY(J1)-U(N1)*H1;
+        A(J1,N1)=A(J1,N1)-H1;
     end
- end
-   end
-end
+
   
-
-% #################################################
-% HASTA ACA ESTOY SEGURO DE QUE ESTA PERFECTO IGUAL QUE EL LIBRO
-% GUIA%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-    % ######################################################### DUDA
-   
+    KK=KODE(N2);
+     
+    if KK==0 | KK==2
+        
+        XY(J1)=XY(J1)+Q(N2)*G2;
+        A(J1,N2)=A(J1,N2)+H2;
     
-%     for J11=1:NN
-%         
-%     KK=KODE(N1);
-% %      
-%     if KK==0 | KK==2
-%         
-%         XY(J11)=XY(J11)+Q(N1(J11)).*G1(J11);
-%         A(J11,N1)=A(J11,N1)+H1;
+    else
+        XY(J1)=XY(J1)-U(N2)*H2;
+        A(J1,N2)=A(J1,N2)-G2;
+    end
+
+    %
 %     
-%     else
-%         XY(J11)=XY(J11)-U(N1(J11)).*H1(J11);
-%         A(J11,N1)=A(J11,N1)-G1;
-%     end
-%     
-%     
-%     KK=KODE(N2);
-% %      
-%     if KK==0 | KK==2
-%         
-%         XY(J11)=XY(J11)+Q(N2(J11)).*G2(J11);
-%         A(J11,N2)=A(J11,N2(J11))+H2;
-%     
-%     else
-%         XY(J11)=XY(J11)-U(N2(J11)).*H2(J11);
-%         A(J11,N2)=A(J11,N2)-G2;
-%     end
-%     
-% HH(J11,J11)=cc;
-% KK=KODE(J11);
+HH(J1,J1)=CC;  % Apply boundary conditions to diagonal term
+KK=KODE(J1);
 % %         
-% %     
-%     if KK==0 | KK==2
-%         
-%         
-%         A(J11,J11)=cc;
-%         
-%     else
-%         
-%         XY(J11)=XY(J11)-U(J11)*cc;
-%     end
+%     
+    if KK==0 | KK==2
+        
+        
+        A(J1,J1)=CC;
+        
+    else
+        
+        XY(J1)=XY(J1)-U(J1)*CC;
+    end
     
-%     end
+    end
+   
+% 
+% % SUBRUTINA NECMOD
+% 
+for I=1:NN+L
+    
+   XI=X(I);
+   YI=Y(I);
+   FF=0;
+   
+   for J1=1:NCI
+       
+      AR=DA(J1);
+      N1=MKJ(J1,1);
+      N2=MKJ(J1,2);
+      N3=MKJ(J1,3);
+       
+      X1=X(N1);
+      X2=X(N2);
+      X3=X(N3);
+      Y1=Y(N1);
+      Y2=Y(N2);
+      Y3=Y(N3);
+      
+      
+      for J3=1:NPI
+         
+         C1=CP(J3,1);
+         C2=CP(J3,2);
+         C3=CP(J3,3);
+          
+         % XP, YP COODINATES OF INTEFRATION POINT 
+         
+         XP=X1*C1+X2*C2+X3*C3;
+         YP=Y1*C1+Y2*C2+Y3*C3;
+         
+         % DX, DY COMPONENTS OR R
+         
+         
+         DX=XI-XP;
+         DY=YI-YP;
+         R=sqrt(DX*DX+DY*DY);
+         
+         
+         % SUMATORIA SOBRE LAS CELDAS USANDO LA FORMULA 2.78.
+         
+         FF=FF-WW(J3)*AR*log(1/R)*(const/(2*pi));
+         D(I)=FF;
+         
+         for j=1:NN
+             
+         XY(j)=XY(j)+FF; % ##########################################
+      end
+      end
+   end
+    
+end
+% 
+% 
+% % SUBRUTINA SOLVER
+% 
+for M=1:NN-1
+    
+   AM=(1/A(M,M));
+   
 
 
+   for J=M+1:NN
+       
+       CD=A(M,J);
+       A(M,J)=CD*AM;
+    
+   end 
+   
+       for N=M+1:NN
+       
+       ALM=A(N,M);
+       
+      
+       for KK=M+1:NN
+           
+           A(N,KK)=A(N,KK)-A(M,KK)*ALM;
+           
+       end
+       end
+end
 
 
+% SUBRUTINA SOLVER
+
+        for M=1:NN-1
+            
+            AM=1/A(M,M);
+            XY(M)=XY(M)*AM;
+            
+            for N=M+1:NN
+                
+                ALM=A(N,M);
+                XY(N)=XY(N)-XY(M)*ALM;
+                XY(NN)=XY(NN)/A(NN,NN);
+                NI=NN;
+                NI=NI-1;
+                
+                
+                if NI~=0
+                    
+                    for J=NI+1:NN
+                   
+                        XY(NI)=XY(NI)-A(NI,J)*XY(J);
+                        
+                    end 
+                end
+                
+            end
+        end
+
+    
+for I=I:NN
+    
+    KK=KODE(I);
+    
+    if KK==0 | KK==2
+       U(I)=XY(I);
+       
+    else
+        
+        Q(I)=XY(I);
+        
+    end
+end
+
+% SUBRUTINA INTERM
+
+for I=NN+1:NN+L
+    
+   U(I)=0;
+   
+   for K=1:NN-1
+   
+       U(I)=U(I)+(GG(I,2*K)+GG(I,(2*K+1)))*Q(K+1);
+       
+       U(I)=U(I)+(GG(I,1)+GG(I,(2*NE)))*Q(1);
+       
+       for K1=1:NN
+          
+           U(I)=U(I)-HH(I,K1)*U(K1);
+           U(I)=U(I)+D(I);
+           
+       end
+   end
+    
+end
 
 tabla2=[zf',x',y']'; % Crea los datos de la tabla "Data for boundary nodes"
 tabla1=[zf',CON,LE']'; % Crea la tabla "Element data"
@@ -299,10 +426,12 @@ fprintf('%6.0f %12.4f %12.4f\n',XYZ);
 disp('--------------------------------------------------')
 fprintf('%8s %12s %12s %12s\n','Nodo 1','Nodo 2','Nodo 3','Área celda');
 fprintf('%8.0f %12.0f %12.0f %12.4f\n',tabla4);
+disp('--------------------------------------------------')
+fprintf('%10s %8s %12s\n','Nodo','U','Q');
+fprintf('%8.0f %12.4f %12.4f\n',[z,U',Q']');
 
 
-% 
- plot(x,y,'ko')
- hold on 
- plot(xint,yint,'bo')
- grid on
+plot(x,y,'ko')
+hold on 
+plot(xint,yint,'bo')
+grid on
